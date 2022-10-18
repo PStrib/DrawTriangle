0268823b936026979c6752ca6453201ae46a301b
PStrib <peter.strib@gmail.com>
2022-10-18 04:18:15 +0100

    Prints not enough layers, and the spaces before it are constant for every pass of the loop

--- src/DrawTriangle.java
+++ src/DrawTriangle.java
@@ -7,10 +7,23 @@ import java.util.Scanner;
 public class DrawTriangle {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the height of your triangle");
         while (!scan.hasNextInt()){
             scan.next();
             System.out.println("Integers only. Please try again");
         }
         int height=scan.nextInt();
+        System.out.println(height);
+
+        String spaces=" ";
+        String exes="X";
+
+        for (int i=height; i>=0; i--){
+            for (int j=1; j<=height+1; j+=2){
+                System.out.print(spaces.repeat(i));
+                System.out.print(exes.repeat(j)+"\n");
+            }
+        }
     }
 }
+
